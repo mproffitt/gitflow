28ace9d93c832c1e86d90f0f40979c13807a971d
Martin Proffitt <mproffitt@plus.net>
2014-03-25 12:25:05 +0000

    Added test pre steps

--- features/steps/bash_steps.rb
+++ features/steps/bash_steps.rb
@@ -9,6 +9,9 @@ Given /^I'm in "([^\"]*)"$/ do |dir|
   FileUtils.cd(dir)
 end
 
-Given(/^I am running GitFlow(?: .*?)? commands$/) do
-
+Given /^I am running GitFlow(?: .*?)? commands$/ do
+    FileUtils.cd '/tmp'
+    FileUtils.rm_rf 'TestRepo'
+    FileUtils.mkdir 'TestRepo'
+    FileUtils.cd 'TestRepo'
 end
